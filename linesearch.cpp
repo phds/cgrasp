817f0bc6038e7de1b12a542edf6bb0fb2adf5a2d
Pedro Dias <phds@cin.ufpe.br>
2015-11-29 20:56:15 -0300

    structured file, can work with vector, can't import module yet

--- linesearch.cpp
+++ linesearch.cpp
@@ -1,19 +1,40 @@
 #include "libs/common.h"
+#include <vector>
 
 using namespace std;
-double lineSearch(double *x,int n,int h,double *l,double *u,int k);
 
+double callAckley(std::vector<double> x){
+	
+	setenv("PYTHONPATH",".",1);
+	Py_Initialize();
 
-int main(){
-	double x[2]= {2,2};
-	int n=2;
-	int h=5;
-	double l[2]={-10,-10};
-	double u[2]={10,10};
-	int k=0;
-	double r=lineSearch(x,n,h,l,u,k);
-	printf("%lf\n",r );
-	return 0;
+	int n = x.size();
+
+	PyObject *list;
+	list = PyList_New(n);
+
+	PyObject *value;
+	for (int i = 0; i < n; i++){
+		value = PyFloat_FromDouble(x[i]);
+		PyList_SetItem(list, i, value);
+	}
+	// double v1, v2;
+	// PyArg_Parse(PyList_GetItem(list, 0), "d", &v1);
+	// PyArg_Parse(PyList_GetItem(list, 1), "d", &v2);
+	// printf("%f %f\n", v1, v2);
+
+	printf("%f\n", PyFloat_AsDouble(PyList_GetItem(list, 0)));
+
+	PyObject* myModuleString = PyString_FromString("ackley");
+	PyObject* myModule = PyImport_Import(myModuleString);
+	if (myModule==NULL) printf("pModule null\n");
+	
+	PyObject* string = PyString_FromString((char*)"ackley");
+	PyObject *result = PyObject_CallMethodObjArgs(myModule, string,list);
+	//PyObject_Print(result, stdout, 0);
+	//printf("\n");
+
+	return PyFloat_AsDouble(result);
 }
 
 /*
@@ -24,63 +45,61 @@ l eh o lower bound
 u eh o upper bound
 k eh o indice
 */
-double lineSearch(double *x,int n,int h,double *l,double *u,int k){
-	double *t[n];
+// double lineSearch(std::vector<double> x,int n,int h,double *l,double *u,int k){
 
-	for(int z=0;z<n;z++){
-		*(t+z) = x+z;
-	}
+// 	//copy of x
+// 	std::vector<double> t(x);
 
-	double * z;
-	z=x;
-	double zk=*(x+k);
+// 	double zk = x[k];
 
-	setenv("PYTHONPATH",".",1);
-	Py_Initialize();
-	PyObject* myModuleString = PyString_FromString((char*)"ackley");
-	PyObject* myModule = PyImport_Import(myModuleString);
+// 	float minF= callAckley(x);
+
+
+
+// 	*(t+k)=(l+k);
+// 	double *tk;
+// 	tk=*(t+k);
+// 	int uk;
+// 	uk=*(u+k);
+// 	int j;
 	
-	PyObject *list,*list1;
-	int i;
-	list = PyList_New(0);
-	Py_INCREF(list);
-	for(i=0;i<n;i++){
-		PyObject* thePair = Py_BuildValue( "d", *(x+i));
-		Py_INCREF(thePair);
-		PyList_Append(list,thePair);
-	}
-	Py_INCREF(list);
-	PyObject *result = PyObject_CallMethod(myModule, "ackley","O",list);
-	//PyObject_Print(result, stdout, 0);
-	//printf("\n");
-
-	float minF= PyFloat_AsDouble(result);
-	*(t+k)=(l+k);
-	double *tk;
-	tk=*(t+k);
-	int uk;
-	uk=*(u+k);
-	int j;
-	
-	while (*tk<=uk){
-		list1 = PyList_New(0);
-		for(j=0;j<n;j++){
-			//cout<<(*t[j])<<endl<<j<<endl;
-			PyObject* thePair = Py_BuildValue( "d", *t[j]);
-			Py_INCREF(thePair);
-			PyList_Append(list1,thePair);
-		}
-		PyObject *result1 = PyObject_CallMethod(myModule, "ackley","O",list1);
-		//PyObject_Print(result1, stdout, 0);
-		//printf("\n");
-		float minF_t= PyFloat_AsDouble(result1);
-		if (minF_t<minF){
-			minF=minF_t;
-			zk=*tk;
-		}
-		tk+=h;
+// 	while (*tk<=uk){
+// 		list1 = PyList_New(0);
+// 		for(j=0;j<n;j++){
+// 			//cout<<(*t[j])<<endl<<j<<endl;
+// 			PyObject* thePair = Py_BuildValue( "d", *t[j]);
+// 			Py_INCREF(thePair);
+// 			PyList_Append(list1,thePair);
+// 		}
+// 		PyObject *result1 = PyObject_CallMethod(myModule, "ackley","O",list1);
+// 		//PyObject_Print(result1, stdout, 0);
+// 		//printf("\n");
+// 		float minF_t= PyFloat_AsDouble(result1);
+// 		if (minF_t<minF){
+// 			minF=minF_t;
+// 			zk=*tk;
+// 		}
+// 		tk+=h;
 		
-	}
+// 	}
 
-return zk;	
+// return zk;	
+// }
+
+int main(){
+
+	double temp[2] = {15, 222};
+
+	int n=2;
+	std::vector<double> x(0);
+	x.insert(x.begin(), temp, temp + n);
+	
+	printf("%lf\n", callAckley(x));
+	// int h=5;
+	// double l[2]={-10,-10};
+	// double u[2]={10,10};
+	// int k=0;
+	// double r=lineSearch(x,n,h,l,u,k);
+	// printf("%lf\n",r );
+	return 0;
 }
