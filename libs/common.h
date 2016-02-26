37c15c5e076890076acab1019bba6076c6b2ee3d
rmf4 <rmf4@cin.ufpe.br>
2016-02-26 15:40:49 -0300

    struct GraspData created

--- libs/common.h
+++ libs/common.h
@@ -1,5 +1,6 @@
 //for general libraries
-
+#include <stddef.h> 
+#include <mpfr.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -7,3 +8,17 @@
 #include "../src/PythonInterface.h"
 
 #define infinity 1000000000000.0
+
+#ifndef GRASPDATA_H
+#define GRASPDATA_H
+struct graspData{
+  std::vector<double> x;
+  int n;
+  double hs;
+  double he;
+  std::vector<double> l;
+  std::vector<double> u;
+  double ro;
+  int k;
+};
+#endif
